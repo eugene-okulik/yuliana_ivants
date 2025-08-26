5c4ee70f92acb8ca20d8541c284d2374d75267ad
yuivantsova <kazakovayvi@gmail.com>
2025-08-26 08:41:51 +0300

    fix key

--- homework/yuliana_ivantsova/task_4.py
+++ homework/yuliana_ivantsova/task_4.py
@@ -10,7 +10,7 @@ print(my_dict['tuple'][-1])
 my_dict['list'].append('Six')
 my_dict['list'].pop(1)
 
-my_dict['dict']['i am a tuple'] = True
+my_dict['dict'][('i am a tuple',)] = True
 my_dict['dict'].pop('three')
 
 my_dict['set'].add(8)
