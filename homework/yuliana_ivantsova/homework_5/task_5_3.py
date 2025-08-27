0b89acad7d5486d2f05cb1b3a7a385fa1f94d31e
yuivantsova <kazakovayvi@gmail.com>
2025-08-27 17:37:39 +0300

    add folder and task 1 2 3

---
+++ homework/yuliana_ivantsova/homework_5/task_5_3.py
@@ -0,0 +1,5 @@
+students = ['Ivanov', 'Petrov', 'Sidorov']
+subjects = ['math', 'biology', 'geography']
+students_string = ', '.join(students)
+subjects_string = ', '.join(subjects)
+print(f'{students_string} study these subjects: {subjects_string}')
