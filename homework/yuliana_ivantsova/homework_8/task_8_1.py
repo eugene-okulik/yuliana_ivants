a61a97c2391d6cd0315833c3f5535cd48a0ba398
yuivantsova <kazakovayvi@gmail.com>
2025-09-17 23:26:18 +0300

    add homework 8

---
+++ homework/yuliana_ivantsova/homework_8/task_8_1.py
@@ -0,0 +1,18 @@
+import random
+
+salary = int(input("Введите желаемую заработную плату: "))
+
+
+def assigning_bonus(salary):
+
+    bonus = random.choice([True, False])
+
+    if bonus:
+        salary += random.randint(1, 1000)
+    else:
+        print("Бонус не получен, увы")
+
+    return salary, bonus
+
+new_salary, bonus = assigning_bonus(salary)
+print(f"{salary}, {bonus} - '${new_salary}'")
---
+++ homework/yuliana_ivantsova/homework_8/task_8_2.py
@@ -0,0 +1,19 @@
+def generator_fibonacci():
+    a, b = 0, 1
+    while True:
+        yield a
+        a, b = b, a + b
+
+def get_num_from_fibonacci(num):
+
+    fib_generator = generator_fibonacci()
+    for i in range(num + 1):
+        result = next(fib_generator)
+    return result
+
+
+print(f"Пятое число из ряда Фибоначчи: {get_num_from_fibonacci(4)}")
+print(f"Двухсотое число из ряда Фибоначчи: {get_num_from_fibonacci(199)}")
+print(f"Тысячное число из ряда Фибоначчи: {get_num_from_fibonacci(999)}")
+print(f"Стотысячное число из ряда Фибоначчи: {get_num_from_fibonacci(9999)}")
+
