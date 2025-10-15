e16fa88c09053ffd5a81025398be185a214e55fc
yuivantsova <kazakovayvi@gmail.com>
2025-10-15 18:04:51 +0300

    add task 12 file

--- homework/yuliana_ivantsova/homework_12/task_12.py
+++ homework/yuliana_ivantsova/homework_12/task_12.py
@@ -1,65 +1,20 @@
-from datetime import datetime, timedelta
+from flowers import Rose, Tulip, Lily
+from bouquet import Bouquet
 
-
-class Flowers:
-
-    def __init__(self, name, color, stem_length, freshness, price, lifespan_days):
-        self.__name = name
-        self.__price = price
-        self.color = color
-        self.stem_length = stem_length
-        self.freshness = freshness
-        self.lifespan_days = lifespan_days
-        self.created_date = datetime.now()
-
-
-    @property
-    def name(self):
-        return self.__name
-
-    @property
-    def price(self):
-        return self.__price
-
-    def __str__(self):
-        return f"{self.__name} ({self.color}) - {self.price} руб."
-
-    def __repr__(self):
-        return f"{self.__name} ({self.color}) - {self.price} руб."
-
-    @price.setter
-    def price(self, value):
-        self.__price = value
-
-    def get_wilting_date(self):
-        return self.created_date + timedelta(days=self.lifespan_days)
-
-
-
-class Rose(Flowers):
-    __name = 'Rose'
-    def __init__(self, color, stem_length, freshness, price, lifespan_days):
-        super().__init__(self.__name, color, stem_length, freshness, price, lifespan_days)
-
-class Lily(Flowers):
-    __name = 'Lily'
-    def __init__(self, color, stem_length, freshness, price, lifespan_days):
-        super().__init__(self.__name, color, stem_length, freshness, price, lifespan_days)
-
-class Tulip(Flowers):
-    __name = 'Tulip'
-    def __init__(self, color, stem_length, freshness, price, lifespan_days):
-        super().__init__(self.__name, color, stem_length, freshness, price, lifespan_days)
-
-
-class Bouquet:
-    def __init__(self, *args):
-        self.flowers_list = list(args)
-
-
-
-rose = Rose('red', '10', '20', '30', 4)
-tulip = Tulip('blue', '10', '20', '30', 3)
-lily = Lily('blue', '10', '20', '30', 3)
+rose = Rose('red', 10, 20, 200)
+tulip = Tulip('blue', 45, 50, 30)
+lily = Lily('blue', 10, 90, 100)
 bouquet = Bouquet(rose, tulip, lily)
+
 print(bouquet.flowers_list)
+print(
+    f"Среднее время увядания: "
+    f"{bouquet.get_average_wilting_time_bouguet()} дней"
+)
+print(f"Общая стоимость: {bouquet.get_total_price()} руб.")
+
+print("Красные цветы:", bouquet.search_by(color='red'))
+print("Цена 100 руб:", bouquet.search_by(price=100))
+print("Свежесть 90%:", bouquet.search_by(freshness=90))
+print("Длина стебля 45 см:", bouquet.search_by(stem_length=45))
+print("Красные и цена 200:", bouquet.search_by(color='red', price=200))
