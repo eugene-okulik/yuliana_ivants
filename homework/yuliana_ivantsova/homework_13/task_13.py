593ec510f8dde96006f72a4f9bad58b58c773dcf
Yuliana Ivantsova <kazakovayvi@gmail.com>
2025-10-18 22:10:23 +0300

    add task 13

--- homework/yuliana_ivantsova/homework_13/task_13.py
+++ homework/yuliana_ivantsova/homework_13/task_13.py
@@ -1,35 +1,34 @@
 import os
 import datetime
 
-dir_path = os.path.dirname(__file__)
-homework_path = os.path.dirname(os.path.dirname(dir_path))
-data_path = os.path.join(homework_path, 'eugene_okulik', 'hw_13', 'data.txt')
+
+current_dir = os.path.dirname(__file__)
+base_dir = os.path.dirname(os.path.dirname(current_dir))
+file_dir = os.path.join(base_dir, 'eugene_okulik', 'hw_13', 'data.txt')
 
 
 def read_file():
-    with open(data_path, 'r', encoding='utf-8') as data_file:
-        for line in data_file:
-            yield line
+    with open(file_dir, 'r') as file:
+        for line in file:
+            datetime_reader(line)
 
 
 def datetime_reader(line):
-    line_part = line.strip().split()
-
-    numer_line = line_part[0].rstrip('.')
-    date_str = ' '.join(line_part[1:3])
+    line_path = line.strip().split()
+    number_line = line_path[0].rstrip('.')
+    date_str = ' '.join(line_path[1:3])
 
     try:
         date = datetime.datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S.%f')
-
-        if numer_line == '1':
+        if number_line == '1':
             new_data = date + datetime.timedelta(weeks=1)
             print(f"Дата через неделю: {new_data}")
 
-        elif numer_line == '2':
+        elif number_line == '2':
             day_index = date.strftime('%A')
             print(f"День недели: {day_index}")
 
-        elif numer_line == '3':
+        elif number_line == '3':
             days_ago = (datetime.datetime.now() - date).days
             print(f"Дней прошло: {days_ago}")
 
@@ -37,11 +36,5 @@ def datetime_reader(line):
         print(f"Ошибка парсинга даты: {e}")
 
 
-def process_dates():
-
-    for line in read_file():
-        datetime_reader(line)
-
-
 if __name__ == "__main__":
-    process_dates()
+    read_file()
